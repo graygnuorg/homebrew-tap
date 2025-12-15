f1a8f2443f44b3e38567b36b239158aadd07f5c8
Sergey Poznyakoff <gray@gnu.org>
2025-12-15 17:27:13 +0000

    Initial commit

---
+++ Formula/pound.rb
@@ -0,0 +1,38 @@
+class Pound < Formula
+  desc "Reverse proxy, load balancer and HTTPS front-end for web servers"
+  homepage "https://github.com/graygnuorg/pound"
+  url "https://github.com/graygnuorg/pound/releases/download/v4.19/pound-4.19.tar.gz"
+  sha256 "11bd62439c4e916488af2956f388fe016fda3ddb3f33e7bf3b666d29d7fd24ca"
+  license "GPL-3.0-or-later"
+
+  depends_on "pkgconf" => :build
+  depends_on "adns"
+  depends_on "lua"
+  depends_on "openssl@3"
+  depends_on "pcre2"
+
+  uses_from_macos "libxcrypt"
+
+  def install
+    system "./configure", *std_configure_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"pound.cfg").write <<~EOS
+      ListenHTTP
+        Address 1.2.3.4
+        Port    80
+        Service
+          Host "www.server0.com"
+          BackEnd
+            Address 192.168.0.10
+            Port    80
+          End
+        End
+      End
+    EOS
+
+    system "#{sbin}/pound", "-f", "#{testpath}/pound.cfg", "-c"
+  end
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# Graygnuorg Tap
+
+This tap provides formula for [pound](https://github.com/graygnuorg/pound) proxy server.
+
+To install it, type:
+
+```sh
+brew install graygnuorg/tap/pound
+```
+
+Or `brew tap graygnuorg/tap` and then `brew install pound`.
+
+Or, in a `brew bundle` `Brewfile`:
+
+```ruby
+tap "graygnuorg/tap"
+brew "pound"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
